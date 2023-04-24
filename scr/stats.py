fd61f20a1eee94db423492d442925bbf2e5449b5
Nina <99652641+samarinanina@users.noreply.github.com>
2023-04-24 18:06:11 +0300

    Create stats.py

---
+++ scr/stats.py
@@ -0,0 +1,12 @@
+class Stats():
+    """статистика"""
+    def __init__(self):
+        """инициализация статистики"""
+        self.reset_stats()
+        self.run_game = True
+        with open("best_score.txt", 'r') as f:
+            self.best_score = int(f.readline())
+    def reset_stats(self):
+        """изменение статистики во время игры"""
+        self.lives_left = 3
+        self.score = 0
