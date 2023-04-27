020472116abebfc0cf53760d01f64d5c7d43de3e
Nina <99652641+samarinanina@users.noreply.github.com>
2023-04-27 20:15:56 +0300

    Create carrot.py

---
+++ scr/carrot.py
@@ -0,0 +1,24 @@
+import pygame
+
+
+class Carrot(pygame.sprite.Sprite):
+    """инициализация моркови"""
+
+    def __init__(self, screen, rabbit):
+        super(Carrot, self).__init__()
+        self.screen = screen
+        self.image = pygame.image.load("images_text/carrot.png")
+        self.rect = self.image.get_rect()
+        self.speed =  5.5
+        self.rect.centerx = rabbit.rect.centerx
+        self.rect.top = rabbit.rect.top
+        self.y = float(self.rect.y)
+
+    def update(self):
+        """перемещение моркови вверх"""
+        self.y -= self.speed
+        self.rect.y = self.y
+
+    def draw_carrot(self):
+        """прорисовка моркови"""
+        self.screen.blit(self.image, self.rect)
