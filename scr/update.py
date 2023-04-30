2523fa58035c252fee3d777bcca4acc023dfc008
Nina <99652641+samarinanina@users.noreply.github.com>
2023-04-30 19:07:21 +0300

    Update update.py

--- scr/update.py
+++ scr/update.py
@@ -1,6 +1,6 @@
 import pygame
-import check
-import controls
+import scr.check
+import scr.controls
 
 def update_screen(bg_color, rabbit, screen, stats, scores, faces, carrot):
     """обновление экрана"""
@@ -23,15 +23,15 @@ def update_carrots(screen, faces, carrots, stats, scores):
         for faces in collisions.values():
             stats.score += 10 * len(faces)
         scores.image_score()
-        check.check_record(stats, scores)
+        scr.check.check_record(stats, scores)
         scores.image_lives()
     if len(faces) == 0:
         carrots.empty()
-        controls.create_army(screen, faces, True)
+        scr.controls.create_army(screen, faces, True)
 
 def update_faces(stats, screen, scores, rabbit, faces, carrots):
     """обновляет позицию лиц"""
     faces.update()
     if pygame.sprite.spritecollideany(rabbit, faces):
-        controls.lost_life(stats, screen, scores, rabbit, faces, carrots)
-    check.faces_check(stats, screen, scores, rabbit, faces, carrots)
+        scr.controls.lost_life(stats, screen, scores, rabbit, faces, carrots)
+    scr.check.faces_check(stats, screen, scores, rabbit, faces, carrots)
