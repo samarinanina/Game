0abcaefc6dd024bf51b9847fa3bc1de575005d82
Nina <99652641+samarinanina@users.noreply.github.com>
2023-04-30 19:05:49 +0300

    Update life.py

--- scr/life.py
+++ scr/life.py
@@ -8,7 +8,7 @@ class Life(Sprite):
     def __init__(self, screen):
         super(Life, self).__init__()
         self.screen = screen
-        self.image = pygame.image.load("images_text/life.png")
+        self.image = pygame.image.load("scr/images_text/life.png")
         self.rect = self.image.get_rect()
         self.screen_rect = screen.get_rect()
         self.center = float(self.rect.centerx)
