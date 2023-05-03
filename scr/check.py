30b64f19239ad4c183d0d60849bf5688e0c82a77
Nina <99652641+samarinanina@users.noreply.github.com>
2023-05-03 20:50:13 +0300

    Update check.py

--- scr/check.py
+++ scr/check.py
@@ -1,4 +1,7 @@
 import scr.controls
+import pygame
+from scr.config import const
+
 
 def faces_check(stats, screen, scores, rabbit, faces, carrots):
     """проверка, что лица дошли до низа экрана"""
@@ -8,9 +11,17 @@ def faces_check(stats, screen, scores, rabbit, faces, carrots):
             scr.controls.lost_life(stats, screen, scores, rabbit, faces, carrots)
             break
 
-def check_record(stats, scores):
+def check_record(stats, scores, screen):
     if stats.score > stats.best_score:
         stats.best_score = stats.score
         scores.image_best_score()
         with open("scr/images_text/best_score.txt", 'w') as f:
             f.write(str(stats.best_score))
+        scores.show_score()
+        if const.new_record:
+            rec_surf = pygame.image.load("scr/images_text/record.png")
+            rec_rect = rec_surf.get_rect(center=(const.record_position))
+            screen.blit(rec_surf, rec_rect)
+            pygame.display.update()
+            pygame.time.delay(const.record_time)
+            const.new_record = False
