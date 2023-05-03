0e58c68a1b3bbffb9cbfcdd279a6c5296c3a10c5
Nina <99652641+samarinanina@users.noreply.github.com>
2023-05-03 20:52:20 +0300

    Update face.py

--- scr/face.py
+++ scr/face.py
@@ -1,4 +1,6 @@
 import pygame
+from scr.config import const
+
 
 class Face(pygame.sprite.Sprite):
     """класс одного недовольного лица"""
@@ -12,18 +14,15 @@ class Face(pygame.sprite.Sprite):
         self.rect.y = self.rect.height
         self.x = float(self.rect.x)
         self.y = float(self.rect.y)
-        self.speed = 0.03
-
-
+        self.speed = const.face_speed
 
     def draw(self):
         """прорисовка лица"""
         self.screen.blit(self.image, self.rect)
 
+    def change_speed(self):
+        self.speed += const.change_face_speed * const.check
+
     def update(self):
         self.y += self.speed
         self.rect.y = self.y
-
-    def change_speed(self):
-        self.speed += 0.01
-        print(self.speed)
